448b8508cd7fa47cd06684d7e6b363b7e997ed5f
huchenlei <chenlei.hu@mail.utoronto.ca>
2023-05-29 16:35:18 -0400

    :bug: Fix default hands

--- process_keypoints.py
+++ process_keypoints.py
@@ -16,15 +16,19 @@ def process_keypoints(nums: List[float], width: int, height: int) -> List[List[f
     base_x = find_min(nums[::3])
     base_y = find_min(nums[1::3])
 
+    normalized = all(abs(num) <= 1.0 for num in nums)
+    x_factor = width if normalized else 1.0
+    y_factor = height if normalized else 1.0
+
     return [
-        [(x-base_x) * width, (y-base_y) * height, c]
+        [(x-base_x) * x_factor, (y-base_y) * y_factor, c]
         for x, y, c in zip(nums[::3], nums[1::3], nums[2::3])
     ]
 
 
 
 if __name__ == '__main__':
-    with open('pose.json', 'r') as f:
+    with open('pose_hand_fixed.json', 'r') as f:
         pose = json.load(f)
         person = pose["people"][0]
         
@@ -33,7 +37,7 @@ if __name__ == '__main__':
         height = pose['canvas_height']
 
         json.dump({
-            'left_hand': process_keypoints(person['hand_left_keypoints_2d'], width, height),
-            'right_hand': process_keypoints(person['hand_right_keypoints_2d'], width, height),
-            'face': process_keypoints(person['face_keypoints_2d'], width, height),
+            'left_hand': process_keypoints(person.get('hand_left_keypoints_2d'), width, height),
+            'right_hand': process_keypoints(person.get('hand_right_keypoints_2d'), width, height),
+            'face': process_keypoints(person.get('face_keypoints_2d'), width, height),
         }, f, indent=4)
