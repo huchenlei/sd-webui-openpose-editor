bc678dccf1a56046d8c16a34c22a0f3f6685f96e
huchenlei <chenlei.hu@mail.utoronto.ca>
2023-06-13 00:04:14 -0400

    :art: Support dark theme

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,17 @@ import { createApp } from 'vue'
 import Antd from 'ant-design-vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
-import 'ant-design-vue/dist/antd.compact.css'
 import NotificationPlugin from './Notification';
 import i18n from './i18n';
+
+const urlParams = new URLSearchParams(window.location.search);
+const theme = urlParams.get('theme');
+if (theme === 'dark') {
+  import('ant-design-vue/dist/antd.dark.css');
+  document.body.classList.add('dark-theme');
+} else {
+  import('ant-design-vue/dist/antd.css');
+}
 
 import './assets/main.css'
 
